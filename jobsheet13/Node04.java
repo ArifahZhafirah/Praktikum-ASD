122b15af97cddb4a0760af37c52b70d407d2ed12
ArifahZhafirah <arifahzhafirah206@gmail.com>
2025-06-27 20:34:55 +0700

    Add files via upload

---
+++ jobsheet13/Node04.java
@@ -0,0 +1,14 @@
+package jobsheet13;
+
+public class Node04 {
+    Mahasiswa04 mahasiswa;
+    Node04 left, right;
+
+    public Node04() {
+    }
+    
+    public Node04(Mahasiswa04 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
